52e5cfddeed8dbfd13d6debdc0c092df218003a0
Tokhir <tohir@mail.ru>
2024-10-27 11:11:41 +0500

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,8 @@
+#print('Please, input your text. ')
+my_string = input('Please, input your text here: ')
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[0])
+print(my_string[-1])
