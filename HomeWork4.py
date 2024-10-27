46b1193f97dc134f9e3cb1b156e80e74def6007d
Tokhir <tohir@mail.ru>
2024-10-27 10:51:54 +0500

    Add files via upload

---
+++ HomeWork4.py
@@ -0,0 +1,10 @@
+immutable_var = 15, 'Mask', True, 8.84, [2,4,6,8,'revolver', False]
+print(immutable_var)
+#immutable_var[2] = 44
+print(immutable_var)
+
+mutable_list = [32, 14, "nobody", 6.42]
+print(mutable_list)
+mutable_list[1] = 16
+mutable_list[2] = 45
+print(mutable_list)
