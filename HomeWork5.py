1d7824737d725fa389cb63cc48886bce14102433
Tokhir <tohir@mail.ru>
2024-10-27 11:13:00 +0500

    Add files via upload

---
+++ HomeWork5.py
@@ -0,0 +1,22 @@
+my_list = ['grape', 'apple', 'peach','plum','banana']
+print(my_list)
+print(my_list[0],my_list[-1])
+print('First & last elements: ',my_list[0:4:3])
+print('Sublist: ',my_list[2:5])
+my_list[2] = 'pineapple'
+print('Modified list: ',my_list)
+print()
+print('- - - - - - - - -')
+print()
+
+my_dict = {"cat":["mushuk",'koshka'], "spoon":"qoshiq", 'hole':'teshik'}
+print('Dictionary: ', my_dict)
+print('Translation for', '"spoon": ',  my_dict['spoon'])
+my_dict['cat']='Kot'
+my_dict.update({'dog':['sobaka', 'pyos']})
+print("Modified dictionary: ",my_dict)
+
+
+
+
+
